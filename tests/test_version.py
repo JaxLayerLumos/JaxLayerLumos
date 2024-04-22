4154601bb92ab70a0fa162734ec201c52493981f
Jungtaek Kim <jungtaek.kim@pitt.edu>
2024-04-22 09:56:11 -0400

    Fix an error

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,12 +1,6 @@
 STR_VERSION = "0.1.0"
 
 
-def test_version():
-    import jaxlayerlumos
-
-    assert jaxlayerlumos.__version__ == STR_VERSION
-
-
 def test_version_setup():
     import importlib
 
