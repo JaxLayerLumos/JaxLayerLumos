7bb7b992391361e7204b36460b2e85039e23db30
Jungtaek Kim <jungtaek.kim@pitt.edu>
2024-04-23 23:41:35 -0400

    Update code

---
+++ examples/show_refractive_indices.py
@@ -0,0 +1,26 @@
+import numpy as np
+import jax.numpy as jnp
+import scipy.constants as scic
+
+from jaxlayerlumos.utils_materials import load_material, interpolate_material
+from jaxlayerlumos.utils_spectra import (
+    get_frequencies_visible_light,
+    get_frequencies_wide_visible_light,
+)
+
+
+if __name__ == "__main__":
+    frequencies = get_frequencies_wide_visible_light()
+    materials = [
+        "Ag",
+        "Al",
+    ]
+
+    for material in materials:
+        data_material = load_material(material)
+        n_k_material = interpolate_material(data_material, frequencies)
+
+        print(material)
+        print(n_k_material)
+        print(n_k_material.shape)
+        print("")
--- jaxlayerlumos/utils_spectra.py
+++ jaxlayerlumos/utils_spectra.py
@@ -12,10 +12,6 @@ def convert_frequencies_to_wavelengths(f):
     Returns:
     - Wavelength in meters. Has the same shape as input f.
     """
-    # Note: In JAX, explicit type and dimension checks like those done in NumPy
-    # might not be as straightforward due to JAX's lazy evaluation model.
-    # If you still need type checks, consider doing them at a higher level
-    # or using JAX's custom JIT-compiled function behaviors for runtime assertions.
 
     wvl = scic.c / f
     return wvl
@@ -37,6 +33,12 @@ def convert_wavelengths_to_frequencies(wavelengths):
 
 def get_frequencies_visible_light(num_wavelengths=1001):
     wavelengths = jnp.linspace(380 * scic.nano, 780 * scic.nano, num_wavelengths)
+    frequencies = convert_wavelengths_to_frequencies(wavelengths)
+
+    return frequencies
+
+def get_frequencies_wide_visible_light(num_wavelengths=1001):
+    wavelengths = jnp.linspace(300 * scic.nano, 900 * scic.nano, num_wavelengths)
     frequencies = convert_wavelengths_to_frequencies(wavelengths)
 
     return frequencies
