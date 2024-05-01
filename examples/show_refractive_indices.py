206f68623ce09906007f4fa3f5bdad0d63389285
Jungtaek Kim <jungtaek.kim@pitt.edu>
2024-04-30 23:00:42 -0400

    Update show_refractive_indices

--- examples/show_refractive_indices.py
+++ examples/show_refractive_indices.py
@@ -1,6 +1,7 @@
 import numpy as np
 import jax.numpy as jnp
 import scipy.constants as scic
+import matplotlib.pyplot as plt
 
 from jaxlayerlumos.utils_materials import (
     get_all_materials,
@@ -10,11 +11,33 @@ from jaxlayerlumos.utils_materials import (
 from jaxlayerlumos.utils_spectra import (
     get_frequencies_visible_light,
     get_frequencies_wide_visible_light,
+    convert_frequencies_to_wavelengths,
 )
 
 
+def plot_refractive_indices(wavelengths, n_k_material):
+    wavelengths = wavelengths / scic.nano
+
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.gca()
+
+    ax.plot(wavelengths, n_k_material[:, 0], linewidth=4, label='n')
+    ax.plot(wavelengths, n_k_material[:, 1], linewidth=4, label='k')
+
+    ax.set_xlim([np.min(wavelengths), np.max(wavelengths)])
+    ax.grid()
+
+    ax.set_xlabel('Wavelengths (nm)', fontsize=18)
+    ax.set_ylabel('n, k', fontsize=18)
+    ax.legend(fontsize=18)
+
+    plt.tight_layout()
+    plt.show()
+
+
 if __name__ == "__main__":
-    frequencies = get_frequencies_wide_visible_light()
+    frequencies = get_frequencies_visible_light()
+    wavelengths = convert_frequencies_to_wavelengths(frequencies)
     materials = get_all_materials()
 
     for material in materials:
@@ -25,3 +48,7 @@ if __name__ == "__main__":
         print(n_k_material)
         print(n_k_material.shape)
         print("")
+
+#        if np.any(n_k_material < 0):
+        if True:
+            plot_refractive_indices(wavelengths, n_k_material)
