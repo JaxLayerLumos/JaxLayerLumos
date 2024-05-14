4c2029a1b549a77e85215c536b48295080e9b713
Jungtaek Kim <jungtaek.kim@pitt.edu>
2024-05-14 14:27:41 -0400

    Update tests

--- tests/test_utils_materials.py
+++ tests/test_utils_materials.py
@@ -30,6 +30,13 @@ LIST_MATERIALS = [
 ]
 
 
+def test_load_json():
+    material_indices, str_directory = utils_materials.load_json()
+
+    assert isinstance(material_indices, dict)
+    assert len(material_indices) == len(LIST_MATERIALS)
+
+
 def test_get_all_materials():
     all_materials = utils_materials.get_all_materials()
 
@@ -69,6 +76,23 @@ def test_load_material_failure():
         str(e.value), "Material Material not found in JaxLayerLumos."
     )
 
+
+def test_interpolate():
+    freqs_values = jnp.array([[100.0, 10.0], [200.0, 20.0], [350, 35.0]])
+    frequencies = jnp.array([100.0, 150.0, 175.0, 210.0, 300.0])
+    frequencies_extrapolation = jnp.array([90.0, 100.0, 150.0, 175.0, 210.0, 300.0, 390.0])
+
+    with pytest.raises(AssertionError):
+        utils_materials.interpolate('abc', frequencies)
+    with pytest.raises(AssertionError):
+        utils_materials.interpolate(freqs_values, 'abc')
+    with pytest.raises(AssertionError):
+        utils_materials.interpolate(freqs_values, frequencies_extrapolation)
+
+    values_interpolated = utils_materials.interpolate(freqs_values, frequencies)
+    values_truth = jnp.array([10.0, 15.0, 17.5, 21.0, 30.0])
+
+    np.testing.assert_allclose(values_interpolated, values_truth)
 
 def test_material_visible_light():
     num_wavelengths = 3456
@@ -117,19 +141,36 @@ def test_material_data_conversion_and_interpolation():
 
     expected_frequency_hz = scic.c / (expected_wavelength_um * 1e-6)
 
-    # Find the row in the data where the frequency matches the expected frequency
-    # We use jnp.isclose to handle floating-point comparison
     row = next(
-        (row for row in data if jnp.isclose(row[0], expected_frequency_hz, atol=1e-6)),
+        (row for row in data if jnp.isclose(row[0], expected_frequency_hz)),
         None,
     )
 
-    assert (
-        row is not None
-    ), f"Data row with frequency {expected_frequency_hz} not found."
+    assert row is not None
 
-    # Extract the actual n and k values from the data
     _, actual_n, actual_k = row
 
     assert jnp.isclose(actual_n, expected_n)
     assert jnp.isclose(actual_k, expected_k)
+
+def test_get_n_k_surrounded_by_air():
+    num_wavelengths = 34
+    materials = ['Ag', 'Au', 'W', 'TiO2', 'Si3N4']
+    frequencies = utils_spectra.get_frequencies_visible_light(
+        num_wavelengths=num_wavelengths
+    )
+
+    n_k = utils_materials.get_n_k_surrounded_by_air(materials, frequencies)
+
+    assert n_k.ndim == 2
+    assert n_k.shape[0] == num_wavelengths
+    assert n_k.shape[1] == len(materials) + 2
+    assert jnp.all(jnp.real(n_k[:, 0]) == 1)
+    assert jnp.all(jnp.imag(n_k[:, 0]) == 0)
+    assert jnp.all(jnp.real(n_k[:, -1]) == 1)
+    assert jnp.all(jnp.imag(n_k[:, -1]) == 0)
+    assert jnp.allclose(n_k[10, 1], 0.13209204 + 2.77618307j)
+    assert jnp.allclose(n_k[20, 2], 0.32581145 + 3.05379202j)
+    assert jnp.allclose(n_k[30, 3], 3.74494455 + 2.78771241j)
+    assert jnp.allclose(n_k[20, 4], 2.39396291 + 2.95673696e-09j)
+    assert jnp.allclose(n_k[10, 5], 2.03410384 + 0.j)
