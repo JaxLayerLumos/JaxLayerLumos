6b396f8c0b85936920aee34e8c2eea04b28f98f5
Jungtaek Kim <jungtaek.kim@pitt.edu>
2024-05-23 10:16:57 -0400

    Add and update figures

--- examples/show_refractive_indices.py
+++ examples/show_refractive_indices.py
@@ -1,28 +1,26 @@
 import numpy as np
-import jax.numpy as jnp
+import os
 import scipy.constants as scic
 import matplotlib.pyplot as plt
 
 from jaxlayerlumos.utils_materials import (
     get_all_materials,
-    load_material,
     interpolate_material,
 )
 from jaxlayerlumos.utils_spectra import (
-    get_frequencies_visible_light,
     get_frequencies_wide_visible_light,
     convert_frequencies_to_wavelengths,
 )
 
 
-def plot_refractive_indices(wavelengths, n_k_material):
+def plot_refractive_indices(material, wavelengths, n_material, k_material):
     wavelengths = wavelengths / scic.nano
 
     fig = plt.figure(figsize=(8, 6))
     ax = fig.gca()
 
-    ax.plot(wavelengths, n_k_material[:, 0], linewidth=4, label="n")
-    ax.plot(wavelengths, n_k_material[:, 1], linewidth=4, label="k")
+    ax.plot(wavelengths, n_material, linewidth=4, label="n")
+    ax.plot(wavelengths, k_material, linewidth=4, label="k")
 
     ax.set_xlim([np.min(wavelengths), np.max(wavelengths)])
     ax.grid()
@@ -32,23 +30,26 @@ def plot_refractive_indices(wavelengths, n_k_material):
     ax.legend(fontsize=18)
 
     plt.tight_layout()
-    plt.show()
+
+    str_directory = "./refractive_indices"
+    if not os.path.exists(str_directory):
+        os.mkdir(str_directory)
+
+    plt.savefig(os.path.join(str_directory, f"{material}.png"))
+    plt.close("all")
 
 
 if __name__ == "__main__":
-    frequencies = get_frequencies_visible_light()
+    frequencies = get_frequencies_wide_visible_light(num_wavelengths=101)
     wavelengths = convert_frequencies_to_wavelengths(frequencies)
     materials = get_all_materials()
 
     for material in materials:
-        data_material = load_material(material)
-        n_k_material = interpolate_material(data_material, frequencies)
+        n_material, k_material = interpolate_material(material, frequencies)
 
         print(material)
-        print(n_k_material)
-        print(n_k_material.shape)
+        print(n_material.shape)
+        print(k_material.shape)
         print("")
 
-        #        if np.any(n_k_material < 0):
-        if True:
-            plot_refractive_indices(wavelengths, n_k_material)
+        plot_refractive_indices(material, wavelengths, n_material, k_material)
