ffc156aa4bd75f2c0f2c5b2dc7e26ddabf1ff1a4
Jungtaek Kim <jungtaek.kim@pitt.edu>
2024-06-18 16:08:46 -0400

    Do black

--- examples/generate_materials_md.py
+++ examples/generate_materials_md.py
@@ -1,23 +1,25 @@
 from jaxlayerlumos import utils_materials
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     all_materials = utils_materials.get_all_materials()
 
-    with open('../MATERIALS.md', 'w') as file_materials:
-        file_materials.write('# Supported Materials')
-        file_materials.write('\n')
-        file_materials.write('\n')
+    with open("../MATERIALS.md", "w") as file_materials:
+        file_materials.write("# Supported Materials")
+        file_materials.write("\n")
+        file_materials.write("\n")
 
-        str_all_material = ', '.join(all_materials)
+        str_all_material = ", ".join(all_materials)
         file_materials.write(f"{str_all_material}")
-        file_materials.write('\n')
-        file_materials.write('\n')
+        file_materials.write("\n")
+        file_materials.write("\n")
 
         for material in all_materials:
             file_materials.write(f"- {material}")
-            file_materials.write('\n')
-            file_materials.write('\n')
-            file_materials.write(f"<p align='center'><img src='examples/refractive_indices/{material}.png' width='400' /></p>")
-            file_materials.write('\n')
-            file_materials.write('\n')
+            file_materials.write("\n")
+            file_materials.write("\n")
+            file_materials.write(
+                f"<p align='center'><img src='examples/refractive_indices/{material}.png' width='400' /></p>"
+            )
+            file_materials.write("\n")
+            file_materials.write("\n")
