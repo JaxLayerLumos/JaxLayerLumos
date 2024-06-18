Potentialy fix overflow

jaxlayerlumos/jaxlayerlumos.py
@@ -54,8 +54,9 @@ def stackrt_base(n_i, d, wvl_i, theta_k):
             dtype=jnp.complex128,
         )
 
+        upper_bound = 650.0 # Jungtaek: I manually chose this number by testing a sufficient number of architecutures.  It might be fixed in future.
         delta = 2 * jnp.pi * n_next * d_next * cos_theta_t / wvl_i
-        delta = jnp.clip(jnp.real(delta), -709.78, 709.78) + 1j * jnp.fmod(jnp.imag(delta), 2 * jnp.pi)
+        delta = jnp.real(delta) + 1j * jnp.clip(jnp.imag(delta), -upper_bound, upper_bound)
         P = jnp.array(
             [[jnp.exp(-1j * delta), 0], [0, jnp.exp(1j * delta)]],
             dtype=jnp.complex128,

tests/test_stackrt_gradients.py
@@ -63,26 +63,16 @@ def test_gradient_stackrt_thickness_Au():
     expected_grad_R_TE = jnp.array(
         [
             0.0,
-            -346.0068112470258,
-            -5.178544282651947e-10,
+            -1.060718452499505e-08,
+            9.604134758745862e-11,
         ]
     )
 
     for elem in grad_R_TE:
         print(elem)
 
-    try:
-        np.testing.assert_allclose(grad_R_TE, expected_grad_R_TE)
-    except:
-        expected_grad_R_TE = jnp.array(
-            [
-                0.0,
-                -346.00681124215345,
-                1.5323520548077508e-09,
-            ]
-        )
+    np.testing.assert_allclose(grad_R_TE, expected_grad_R_TE)
 
-        np.testing.assert_allclose(grad_R_TE, expected_grad_R_TE)
 
 def test_gradient_stackrt_thickness_TiO2_W_SiO2():
     num_wavelengths = 5