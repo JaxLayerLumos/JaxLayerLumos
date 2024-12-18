c972213920def836b2362ded1b659ba517421e22
mil152 <mil152@htc-1024-n0.crc.pitt.edu>
2024-12-18 12:38:25 -0500

    remove assert in util

--- jaxlayerlumos/utils_materials.py
+++ jaxlayerlumos/utils_materials.py
@@ -129,8 +129,8 @@ def interpolate(freqs_values, frequencies):
 
     freqs, values = freqs_values.T
 
-    assert jnp.min(freqs) <= jnp.min(frequencies) + 1
-    assert jnp.max(frequencies) <= jnp.max(freqs)
+    # assert jnp.min(freqs) <= jnp.min(frequencies) + 1
+    # assert jnp.max(frequencies) <= jnp.max(freqs)
 
     values_interpolated = jnp.interp(
         frequencies,
