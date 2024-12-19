92550c75b1b411a16c4af5c2ea672729c83d52b8
Jungtaek Kim <jungtaek.kim@pitt.edu>
2024-12-19 15:54:40 -0500

    Update an example

--- examples/calculate_gradients.py
+++ examples/calculate_gradients.py
@@ -4,10 +4,8 @@ import jax
 from jaxlayerlumos import stackrt
 from jaxlayerlumos.utils_materials import get_n_k_surrounded_by_air
 from jaxlayerlumos.utils_spectra import get_frequencies_visible_light
-from jaxlayerlumos.utils_layers import (
-    get_thicknesses_surrounded_by_air,
-    convert_nm_to_m,
-)
+from jaxlayerlumos.utils_layers import get_thicknesses_surrounded_by_air
+from jaxlayerlumos.utils_units import convert_nm_to_m
 
 
 if __name__ == "__main__":
@@ -20,7 +18,7 @@ if __name__ == "__main__":
 
     def R_TE(inputs):
         layers = convert_nm_to_m(get_thicknesses_surrounded_by_air(inputs))
-        R_TE, _, _, _ = stackrt(n_k, layers, frequencies, thetas=angles)
+        R_TE, _, _, _ = stackrt(n_k, layers, frequencies, angles)
         return R_TE[0, 0]
 
     gradients_R_TE = jax.grad(R_TE)(thicknesses)
