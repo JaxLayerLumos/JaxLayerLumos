8ebf14e97f8d6f2ae8666fd7160caf0f7fb3953c
Jungtaek Kim <jungtaek.kim@pitt.edu>
2025-01-04 13:05:22 -0500

    Update all

--- benchmarking/compare_methods.py
+++ benchmarking/compare_methods.py
@@ -35,14 +35,7 @@ def run_simulation(method, frequencies, thicknesses, n_k, angle):
     return R_TE, R_TM, T_TE, T_TM, time_consumed
 
 
-def compare_simulation(frequencies, thicknesses, n_k, angle):
-    methods = onp.array([
-        'ansys',
-        'tmm_fast',
-        'jaxlayerlumos_old',
-        'jaxlayerlumos',
-    ])
-
+def compare_simulation(methods, frequencies, thicknesses, n_k, angle):
     Rs_TE = []
     Rs_TM = []
     Ts_TE = []
@@ -63,8 +56,6 @@ def compare_simulation(frequencies, thicknesses, n_k, angle):
     Ts_TE = onp.array(Ts_TE)
     Ts_TM = onp.array(Ts_TM)
     times_consumed = onp.array(times_consumed)
-
-    print(Rs_TE.shape, Rs_TM.shape, Ts_TE.shape, Ts_TM.shape, times_consumed.shape)
 
     for ind_1 in range(0, methods.shape[0]):
         for ind_2 in range(ind_1 + 1, methods.shape[0]):
@@ -91,109 +82,96 @@ def compare_simulation(frequencies, thicknesses, n_k, angle):
             except:
                 print('T_TM', methods[ind_1], methods[ind_2])
 
+    return Rs_TE, Rs_TM, Ts_TE, Ts_TM, times_consumed
 
-def compare_simulations(num_tests, use_zero_angle, use_thick_layers):
+
+def compare_simulations_layer(methods, num_layers, num_tests, use_zero_angle, use_thick_layers):
     wavelengths = onp.linspace(300e-9, 900e-9, 101)
     frequencies = utils_units.get_light_speed() / wavelengths
 
     all_materials = utils_materials.get_all_materials()
 
+    random_state = onp.random.RandomState(42)
+
+    materials_layer = []
+    thicknesses_layer = onp.zeros((0, num_layers + 1))
+    Rs_TE_layer = onp.zeros((methods.shape[0], 0, wavelengths.shape[0]))
+    Rs_TM_layer = onp.zeros((methods.shape[0], 0, wavelengths.shape[0]))
+    Ts_TE_layer = onp.zeros((methods.shape[0], 0, wavelengths.shape[0]))
+    Ts_TM_layer = onp.zeros((methods.shape[0], 0, wavelengths.shape[0]))
+    times_consumed_layer = onp.zeros((methods.shape[0], 0))
+
+    for _ in range(0, num_tests):
+        materials = random_state.choice(all_materials, num_layers)
+        materials = onp.concatenate([['Air'], materials], axis=0)
+
+        angle = 0.0 if use_zero_angle else random_state.uniform(0.0, 89.9)
+
+        n_k = []
+        thicknesses = []
+
+        for material in materials:
+            n_material, k_material = utils_materials.interpolate_material_n_k(
+                material, jnp.array(frequencies)
+            )
+            n_k_material = n_material + 1j * k_material
+
+            thickness_material = (
+                random_state.uniform(5.0, 100.0)
+                if use_thick_layers
+                else random_state.uniform(0.01, 10.0)
+            )
+            thickness_material *= utils_units.get_nano()
+
+            n_k.append(n_k_material)
+            thicknesses.append(thickness_material)
+
+        thicknesses = onp.array(thicknesses)
+        n_k = onp.array(n_k)
+
+        thicknesses[0] = 0.0
+        thicknesses[-1] = 0.0
+
+        Rs_TE, Rs_TM, Ts_TE, Ts_TM, times_consumed = compare_simulation(
+            methods, frequencies, thicknesses, n_k, angle)
+
+        thicknesses = onp.expand_dims(thicknesses, axis=0)
+        Rs_TE = onp.expand_dims(Rs_TE, axis=1)
+        Rs_TM = onp.expand_dims(Rs_TM, axis=1)
+        Ts_TE = onp.expand_dims(Ts_TE, axis=1)
+        Ts_TM = onp.expand_dims(Ts_TM, axis=1)
+        times_consumed = onp.expand_dims(times_consumed, axis=1)
+
+        materials_layer.append(materials)
+        thicknesses_layer = onp.concatenate([thicknesses_layer, thicknesses], axis=0)
+        Rs_TE_layer = onp.concatenate([Rs_TE_layer, Rs_TE], axis=1)
+        Rs_TM_layer = onp.concatenate([Rs_TM_layer, Rs_TM], axis=1)
+        Ts_TE_layer = onp.concatenate([Ts_TE_layer, Ts_TE], axis=1)
+        Ts_TM_layer = onp.concatenate([Ts_TM_layer, Ts_TM], axis=1)
+        times_consumed_layer = onp.concatenate([times_consumed_layer, times_consumed], axis=1)
+
+    materials_layer = onp.array(materials_layer)
+    print(materials_layer.shape, thicknesses_layer.shape, Rs_TE_layer.shape, Rs_TM_layer.shape, Ts_TE_layer.shape, Ts_TM_layer.shape, times_consumed_layer.shape)
+    print(np.mean(times_consumed_layer, axis=1))
+
+
+def compare_simulations(num_tests, use_zero_angle, use_thick_layers):
+    methods = onp.array([
+        'ansys',
+        'tmm_fast',
+        'jaxlayerlumos_old',
+        'jaxlayerlumos',
+    ])
+
 #    num_layerss = onp.array([2, 4, 6, 8, 10, 12, 14, 16])
     num_layerss = onp.array([2, 4])
 
-    random_state = onp.random.RandomState(42)
-
     for num_layers in num_layerss:
-        for _ in range(0, num_tests):
-            materials = random_state.choice(all_materials, num_layers)
-
-            angle = 0.0 if use_zero_angle else random_state.uniform(0.0, 89.9)
-
-            n_k_air = onp.ones_like(frequencies, dtype=onp.complex128)
-            thickness_air = 0.0
-
-            n_k = [n_k_air]
-            thicknesses = [thickness_air]
-
-            for material in materials:
-                n_material, k_material = utils_materials.interpolate_material_n_k(
-                    material, jnp.array(frequencies)
-                )
-                n_k_material = n_material + 1j * k_material
-
-                thickness_material = (
-                    random_state.uniform(5.0, 100.0)
-                    if use_thick_layers
-                    else random_state.uniform(0.01, 10.0)
-                )
-                thickness_material *= utils_units.get_nano()
-
-                n_k.append(n_k_material)
-                thicknesses.append(thickness_material)
-
-            thicknesses = onp.array(thicknesses)
-            n_k = onp.array(n_k)
-
-            thicknesses[-1] = 0.0
-
-            compare_simulation(frequencies, thicknesses, n_k, angle)
-
-            '''
-            # Compare old and new
-            is_close_R_TE = onp.allclose(R_TE_old, R_TE_new, rtol=1e-5)
-            is_close_T_TE = onp.allclose(T_TE_old, T_TE_new, rtol=1e-5)
-            is_close_R_TM = onp.allclose(R_TM_old, R_TM_new, rtol=1e-5)
-            is_close_T_TM = onp.allclose(T_TM_old, T_TM_new, rtol=1e-5)
-
-            if not (
-                is_close_R_TE and is_close_T_TE and is_close_R_TM and is_close_T_TM
-            ):
-                print(f"Mismatch detected")
-                print(f"materials {materials}")
-                print(f"thicknesses {thicknesses}")
-                print(f"angle {angle}")
-
-                # Calculate Lumerical results
-                Rs, Rp, Ts, Tp = compute_properties_via_stackrt(
-                    onp.array(thicknesses),
-                    onp.array(n_k).T,
-                    onp.array(frequencies),
-                    angle_of_incidence=onp.array([angle]),
-                )
-
-                R_TE_lum = onp.squeeze(onp.array(Rs), axis=1)
-                R_TM_lum = onp.squeeze(onp.array(Rp), axis=1)
-                T_TE_lum = onp.squeeze(onp.array(Ts), axis=1)
-                T_TM_lum = onp.squeeze(onp.array(Tp), axis=1)
-
-                if not is_close_R_TE:
-                    print("R_TE NOT MATHCED")
-                    print(f"Old: {R_TE_old}")
-                    print(f"New: {R_TE_new}")
-                    print(f"Lumerical: {R_TE_lum}\n")
-
-                if not is_close_T_TE:
-                    print("T_TE NOT MATHCED")
-                    print(f"Old: {T_TE_old}")
-                    print(f"New: {T_TE_new}")
-                    print(f"Lumerical: {T_TE_lum}\n")
-
-                if not is_close_R_TM:
-                    print("R_TM NOT MATHCED")
-                    print(f"Old: {R_TM_old}")
-                    print(f"New: {R_TM_new}")
-                    print(f"Lumerical: {R_TM_lum}\n")
-
-                if not is_close_T_TM:
-                    print("T_TM NOT MATHCED")
-                    print(f"Old: {T_TM_old}")
-                    print(f"New: {T_TM_new}")
-                    print(f"Lumerical: {T_TM_lum}\n")
-            '''
+        compare_simulations_layer(methods, num_layers, num_tests, use_zero_angle, use_thick_layers)
 
 
 if __name__ == "__main__":
-    num_tests = 1
+    num_tests = 10
 
     compare_simulations(num_tests=num_tests, use_zero_angle=True, use_thick_layers=False)
     compare_simulations(num_tests=num_tests, use_zero_angle=True, use_thick_layers=True)
