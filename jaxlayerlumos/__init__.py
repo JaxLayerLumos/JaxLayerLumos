ccd343b925055f5ea0bdb91462e6b5a6c64e5df0
Jungtaek Kim <jungtaek.kim@pitt.edu>
2025-03-23 21:44:19 -0400

    Update __init__.py

--- jaxlayerlumos/__init__.py
+++ jaxlayerlumos/__init__.py
@@ -8,14 +8,6 @@ from jaxlayerlumos.jaxlayerlumos import stackrt_eps_mu
 from jaxlayerlumos.wrappers import stackrt_n_k_0
 from jaxlayerlumos.wrappers import stackrt_n_k_45
 
-from jaxlayerlumos.utils import utils_layers
-from jaxlayerlumos.utils import utils_position
-from jaxlayerlumos.utils import utils_radio_frequency
-from jaxlayerlumos.utils import utils_units
-from jaxlayerlumos.utils import utils_materials
-from jaxlayerlumos.utils import utils_radar_materials
-from jaxlayerlumos.utils import utils_spectra
-
 
 stackrt = stackrt_n_k
 stackrt0 = stackrt_n_k_0
