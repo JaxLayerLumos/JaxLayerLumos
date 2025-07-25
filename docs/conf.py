bb6a5ba8e2f90b505a023c05aa1c31dd0d3148c9
Jungtaek Kim <jungtaek.kim@pitt.edu>
2025-07-25 16:19:49 -0400

    Add docs

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'JaxLayerLumos'
+copyright = '2025, Mingxuan Li, Jungtaek Kim, and Paul W. Leu'
+author = 'Mingxuan Li, Jungtaek Kim, and Paul W. Leu'
+release = 'v0.3.3'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
